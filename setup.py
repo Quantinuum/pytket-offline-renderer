d003fb0d9f31f3a36f8d8f221a5b09c80e7e9626
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2026-09-25 09:04:57 +0100

    Update version to 0.1.1. (#25)

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     author="TKET development team",
     author_email="tket-support@cambridgequantum.com",
     python_requires=">=3.9",
-    version="0.0.9",
+    version="0.1.1",
     project_urls={
         "Documentation": "https://cqcl.github.io/tket/pytket/api/index.html",
         "Source": "https://github.com/CQCL/pytket-offline-renderer",
